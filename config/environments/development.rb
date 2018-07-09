21233ea871eebf3c0145d0bf7113d1ad858f4b31
Melanie Santos <santosmelan@gmail.com>
2018-07-09 20:02:15 +0800

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,10 +48,29 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.credentials.aws[:email_provider_username],
+    password: Rails.application.credentials.aws[:email_provider_password]
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
