58339d55ef618a24581bc1d2e643e3cf3dcd0fd5
Melanie Santos <santosmelan@gmail.com>
2018-07-17 16:37:40 +0800

    mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,30 @@
 class VisitorsController < ApplicationController
 
-	def new
-		@owner = Owner.new
-	end
+  def new
+    @visitor = Visitor.new
+  end
+
+  def create
+    @visitor = Visitor.new(secure_params)
+    if @visitor.valid?
+      @visitor.subscribe
+      flash[:notice] = "Signed up #{@visitor.email}."
+    else
+      render :new
+    #   flash[:alert] = []
+	  	# @visitor.errors.full_messages.each do |msg|
+	  	# 	flash[:alert] << msg
+	  	# end
+    end
+
+    redirect_to root_path
+	  
+  end
+
+  private
+
+  def secure_params
+    params.require(:visitor).permit(:email)
+  end
 
 end
