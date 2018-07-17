9c7e30a2f3dcaf285008cc34d58804b7d1396658
Melanie Santos <santosmelan@gmail.com>
2018-07-17 12:14:06 +0800

    contact form

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,28 @@
+class ContactsController < ApplicationController
+
+	def new
+	  @contact = Contact.new
+	end
+
+	def create
+	  @contact = Contact.new(secure_params)
+
+	  if @contact.valid?
+	    flash[:notice] = "Message sent from #{@contact.name}."
+	    redirect_to root_path
+	  else
+	  	flash[:alert] = []
+	  	@contact.errors.full_messages.each do |msg|
+	  		flash[:alert] << msg
+	  	end
+		redirect_to new_contact_path
+	  end
+	end
+
+	private
+
+	def secure_params
+	  params.require(:contact).permit(:name, :email, :content)
+	end
+
+end
